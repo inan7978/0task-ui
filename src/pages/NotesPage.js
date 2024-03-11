da0b79671d8ffd01e2eb48ed044f19bad6ddbc4e
Ismailov <inan7978@icloud.com>
2024-03-11 17:34:32 -0400

    working on the notes

--- src/pages/NotesPage.js
+++ src/pages/NotesPage.js
@@ -1,5 +1,6 @@
 function NotesPage() {
-  const temp = `Oh boy:
+  const temp = [
+    `Oh boy:
     
     * This and that 1
     * This and that 2
@@ -11,19 +12,59 @@ function NotesPage() {
     * sdafasdfaf
     * dsafsadfasd
     * dafdfsdfa
-    * sdafsadfr`;
+    * sdafsadfr`,
+    `Oh boy:
+    
+  * This and that 1
+  * This and that 2
+  * This and that 3
+  * Another ont?
+  * Holy cow!
+  * dsfadsfas
+  * fasdfsdf
+  * 
+  * sdafasdfaf
+  * dsafsadfasd
+  * dafdfsdfa
+  * xxcsccsd
+  * sasadS
+  * 
+  * 
+  * 
+  * dasd
+  * sdafsadfr`,
+    `Oh boy:
+    
+  * This and that 1
+  * This and that 2
+  * This and that 3
+  * Another ont? Why not? They have already taken our gold.
+  * Holy cow!
+  * dsfadsfas
+  * fasdfsdf
+  * sdafasdfaf
+  * dsafsadfasd
+  * dafdfsdfa
+  * sdafsadfr`,
+  ];
+
+  const notes = temp.map((note) => {
+    return <Note temp={note} />;
+  });
 
   return (
-    <div>
-      <Note temp={temp} />
+    <div className="container mx-auto flex flex-col justify-center items-center">
+      <h1 className="text-white text-3xl font-bold my-10">Your notes</h1>
+      <div className="flex-col flex justify-center gap-5 sm:flex-row">
+        {notes}
+      </div>
     </div>
   );
 }
 
 function Note({ temp }) {
   return (
-    <div className="container flex flex-col justify-start w-1/4 max-h-72 p-5 bg-white rounded overflow-hidden">
-      <h1 className="text-2xl font-bold mb-5">Title</h1>
+    <div className="flex flex-col justify-start max-h-72 p-5 mb-5 bg-white rounded overflow-hidden">
       <pre>{temp}</pre>
     </div>
   );
