d17a61dbbda846b39421e4b0d5567c9ddc9d1bd4
Ismailov <inan7978@icloud.com>
2024-03-13 17:06:25 -0400

    making progress

--- src/pages/NotesPage.js
+++ src/pages/NotesPage.js
@@ -3,7 +3,6 @@ import { useSelector } from "react-redux";
 
 function NotesPage() {
   const [notes, setNotes] = useState([""]);
-  const [newNote, setNewNote] = useState(false);
   const token = useSelector((state) => state.counter.accessToken);
   useEffect(() => {
     getNotes(token);
@@ -22,6 +21,27 @@ function NotesPage() {
     setNotes(data.notes);
   }
 
+  async function newNote(token) {
+    const addNote = await fetch("http://localhost:3001/new-note", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({ description: "" }),
+    });
+
+    const data = await addNote.json();
+
+    if (data.status === -1) {
+      console.log("A server error has occured. The new note was not created.");
+      getNotes(token);
+    } else {
+      getNotes(token);
+      console.log(data);
+    }
+  }
+
   const mappedNotes = notes
     ? notes.map((note) => {
         return <Note note={note} key={note._id} />;
@@ -29,23 +49,53 @@ function NotesPage() {
     : null;
 
   return (
-    <div className="container mx-auto flex flex-col justify-center items-center max-w-screen-xl">
-      {/* <h1 className="text-white text-3xl font-bold my-10">{`${notes.length} notes`}</h1> */}
-      <div className="flex gap-5 my-10">
-        <input placeholder="Search notes..." />
-        <button className="p-2 text-white bg-green-500">New Note</button>
-      </div>
-      <div className="flex-col flex flex-wrap justify-center gap-5 sm:flex-row">
-        {notes.length >= 1 ? mappedNotes : null}
+    <div>
+      <div className="container mx-auto flex flex-col justify-center items-center max-w-screen-xl">
+        {/* <h1 className="text-white text-3xl font-bold my-10">{`${notes.length} notes`}</h1> */}
+        <div className="flex gap-5 my-10">
+          <input placeholder="Search notes..." />
+          <button
+            onClick={() => {
+              newNote(token);
+            }}
+            className="p-2 text-white bg-green-500"
+          >
+            New Note
+          </button>
+        </div>
+        <div className="flex-col flex flex-wrap justify-center gap-5 sm:flex-row">
+          {notes.length >= 1 ? mappedNotes : null}
+        </div>
       </div>
     </div>
   );
 }
 
 function Note({ note }) {
+  const [editing, setEditing] = useState(false);
+
   return (
-    <div className="flex flex-col justify-start max-w-[95vw] sm:max-w-[300px] p-5 mb-5 bg-white rounded overflow-hidden">
-      <pre style={{ "white-space": "pre-wrap" }}>{note.description}</pre>
+    <>
+      {editing && <EditNote note={note} />}
+      <div
+        onClick={() => {
+          console.log(`Note ${note._id} clicked`);
+          setEditing(!editing);
+        }}
+        className="flex flex-col justify-start max-w-[95vw] min-w-36 sm:max-w-[300px] p-5 mb-5 bg-white rounded overflow-hidden"
+      >
+        <pre style={{ "white-space": "pre-wrap" }}>{note.description}</pre>
+      </div>
+    </>
+  );
+}
+
+function EditNote({ note }) {
+  const [value, setValue] = useState(note.description);
+
+  return (
+    <div className="absolute bg-black opacity-85 flex justify-center">
+      <textarea value={value} onChange={(e) => setValue(e.target.value)} />
     </div>
   );
 }
