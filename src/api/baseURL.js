940d5967d0e103fd97211db8b425eaebf400b374
Ismailov <inan7978@icloud.com>
2024-03-25 16:23:51 -0400

    forgot to change the BASE_URL for the endpoints back to the hosted backend

--- src/api/baseURL.js
+++ src/api/baseURL.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:3001";
+export const BASE_URL = "https://jwt-auth-webdev-simplified.onrender.com";
 
 // use http://localhost:3001 for local testing
 // https://jwt-auth-webdev-simplified.onrender.com for prod
