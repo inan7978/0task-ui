aefe1687a0d41c481a8097c4f106c8d7e80c286c
Ismailov <Iismailov@hospiceatcharlotte.net>
2024-05-28 10:55:16 -0400

    editted some descriptors

--- src/api/baseURL.js
+++ src/api/baseURL.js
@@ -2,4 +2,4 @@ export const BASE_URL = "https://jwt-auth-webdev-simplified.onrender.com";
 // export const BASE_URL = "http://localhost:3001";
 
 // use http://localhost:3001 for local testing
-// https://jwt-auth-webdev-simplified.onrender.com for prod
+// https://jwt-auth-webdev-simplified.onrender.com for production
