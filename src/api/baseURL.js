976a3bd0d70b4ab3e142b6e7b023306b57a41365
Ismailov <Iismailov@hospiceatcharlotte.net>
2024-05-30 14:42:05 -0400

    switched the base URL to production

--- src/api/baseURL.js
+++ src/api/baseURL.js
@@ -1,5 +1,5 @@
-// export const BASE_URL = "https://jwt-auth-webdev-simplified.onrender.com";
-export const BASE_URL = "http://localhost:3001";
+export const BASE_URL = "https://jwt-auth-webdev-simplified.onrender.com";
+// export const BASE_URL = "http://localhost:3001";
 
 // use http://localhost:3001 for local testing
 // https://jwt-auth-webdev-simplified.onrender.com for production
